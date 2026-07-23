798947d21029d25da4a3a4e86c33075168751574
Yunus Büyükkafes <150488573+yunusbuyukkafes@users.noreply.github.com>
2026-07-23 23:32:05 +0300

    add float data type and VAT calculation example

---
+++ 02_python_basics_variables/float.py
@@ -0,0 +1,23 @@
+# calculate 
+a= 5.2
+b= 10.5
+sum = a + b
+print("sum is", sum)
+abstract = b / a
+print("abstract is", abstract)
+
+# decimal precision
+print (0.1 + 0.2) # 0.30000000000000004
+
+# round
+sum = 0.1 + 0.2
+print("sum is", sum)
+
+rounded_sum = round(sum, 2)
+print("rounded sum is", rounded_sum)
+
+# Calculate 20% VAT
+price = float(input("Enter the price of the product: "))
+print (price)
+price_with_vat = price + (price * 0.2)
+print("Price with VAT is", price_with_vat)
