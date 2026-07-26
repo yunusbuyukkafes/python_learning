fe9ebf530648104bb75e24a24e9f9c12b4a7bd3e
Yunus Büyükkafes <150488573+yunusbuyukkafes@users.noreply.github.com>
2026-07-26 21:56:23 +0300

    add type checking and type conversion examples

---
+++ 02_python_basics_variables/type_conversion.py
@@ -0,0 +1,16 @@
+x = 10
+print(type(x)) 
+
+x ="10"
+print(type(x))
+print(type(int(x)))
+print(type(float(x)))
+
+number =input("Enter a number: ")
+print(number)
+print(type(number)) # int or str
+print(type(int(number)))
+
+number =int(input("Enter a number: "))
+print(number)
+print(type(number))  
